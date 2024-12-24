fe793688a7bf7a0d0c8db66c755ed0f2bb164c12
Makoto Ogata <ogatamakoto1976@gmail.com>
2024-12-24 23:40:27 +0900

    Added animation to slowly display the text

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { Background } from "./background/Background";
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
 
 function App() {
 	return (
@@ -19,13 +19,27 @@ const wrapper = css({
 	height: "100vh",
 });
 
+const textAnimation = keyframes({
+	"0%": {
+		opacity: 0,
+	},
+	"50%": {
+		opacity: 0.5,
+	},
+	"100%": {
+		opacity: 1,
+	},
+});
+
 const message = css({
+	animation: `3s ease-in ${textAnimation}`,
 	color: "#fff",
 	position: "absolute",
 	inset: "0",
 	margin: "auto",
 	width: "max-content",
 	height: "max-content",
+	textShadow: "0px 0px 27px rgba(137, 141, 145, 0.76)",
 });
 
 export default App;
